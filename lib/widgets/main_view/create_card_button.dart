a054a7ef99e561fabd1b76f814a0f9d936cd8594
Tonka <rmcmaster05@gmail.com>
2025-06-05 09:54:04 +0200

    feat: add navigation to card create screen

--- lib/widgets/main_view/create_card_button.dart
+++ lib/widgets/main_view/create_card_button.dart
@@ -4,14 +4,16 @@ import 'menu_button.dart';
 class CreateCardButton extends StatelessWidget {
   const CreateCardButton({super.key});
 
+  void handleNagivation(BuildContext context) {
+    Navigator.of(context).pushNamed('/create');
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomButton(
       color: Colors.blueAccent,
       label: 'CREATE CARD',
-      onPressed: () {
-        // TODO
-      },
+      onPressed: () => handleNagivation(context),
     );
   }
 }
